2abcd5316065caaca50cd726eb2aa8a755def168
Lars Larsson <lars.la@gmail.com>
2012-06-28 23:40:49 +0200

    trying to fix some issues with sockets not being properly closed but staying in TIME_WAIT

--- readthread.py
+++ readthread.py
@@ -1,23 +1,27 @@
 import multiprocessing
+import time
 
 class ReadThread(multiprocessing.Process):
     def __init__(self, id, fifoname, queue):
         print "Initialized ReadThread id " + str(id)
         self.quit = False
-        self.id = id
+        self.tid = id
         self.fifoname = fifoname
         self.queue = queue
 
         multiprocessing.Process.__init__(self)
 
     def run(self):
-        print "Running ReadThread id " + str(id)
+        print str(self.tid) + "Running ReadThread id "
         self.f = open(self.fifoname, 'r')
         while self.quit is False:
             line = self.f.readline()
-            if not '\n' in line: continue
+            if not '\n' in line:
+                time.sleep(0.01)
+                continue
             if not 'PROCESS_SERVICE_CHECK_RESULT' in line: continue
-            print str(self.id) + " | Putting event on queue: " + str(line)
+            while self.queue.full():
+                time.sleep(1)
             self.queue.put(line)
 
     def quit(self):
--- sendthread.py
+++ sendthread.py
@@ -1,26 +1,37 @@
 import multiprocessing
 from pynsca import NSCANotifier
 import re
+import time
 
 class SendThread(multiprocessing.Process):
     def __init__(self, id, queue, nsca_host, nsca_port=5667, nsca_crypt=1, password=None):
         print "Initialized SendThread id " + str(id)
         self.quit = False
-        self.id = id
+        self.tid = id
         self.queue = queue
         self.nsca = NSCANotifier(nsca_host, monitoring_port=nsca_port, encryption_mode=nsca_crypt, password=password)
 
         multiprocessing.Process.__init__(self)
 
     def run(self):
-        print "Running SendThread id " + str(id)
+        print str(self.tid) + " | Running SendThread id "
         while self.quit is False:
-            line = self.queue.get()
-            print str(self.id) + " | Received event: " + str(line)
-            (host, service, code, output) = self.parse(line)
-            print host + " " + service + " " + str(code) + " " + output
-            self.nsca.svc_result(host, service, code, output)
+            if self.queue.empty():
+                time.sleep(1)
+                continue
+            else:
+                line = self.queue.get()
+            (t, host, service, code, output) = self.parse(line)
+            if t > (int(time.time()) - 60):
+                self.nsca.svc_result(host, service, code, output)
+            else:
+                print str(self.tid) + " | " + str(t) + " " + host + ";" + service + ";" + str(code) + ";" + output + " | Too old, dropping"
+            time.sleep(0.001)
+        print str(self.tid) + " | Stopping loop"
 
     def parse(self, line):
         r = re.match(r'(?P<time>[0-9]+)\sPROCESS_SERVICE_CHECK_RESULT;(?P<host>\S+);(?P<service>\S+);(?P<code>[0-9]+);(?P<output>.+)\n', line)
-        return (r.group("host"), r.group("service"), int(r.group("code")), r.group("output"))
+        return (int(r.group("time")), r.group("host"), r.group("service"), int(r.group("code")), r.group("output"))
+
+    def quit(self):
+        self.quit = True
